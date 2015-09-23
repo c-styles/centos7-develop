141f24bca9d36b6c1eb50403e1206162c3022b1a
Eisuke Kozu <eisuke@c-styles.jp>
2015-09-23 18:17:36 +0900

    Berksfile追加

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'centos7-develop'
+maintainer       'c-styles'
+maintainer_email 'eisuke@c-styles.jp'
+license          'All rights reserved'
+description      'Learning Material for CentOS7'
+long_description ''
+version          '0.1.0'
